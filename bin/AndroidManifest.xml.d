f071ee922a1bf4cc8b9fd622e036501963dfd74a
Vimarsh Sharma <vimarsh.sharma@mphasis.com>
2014-05-28 12:47:46 +0530

    Hello

---
+++ bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/Users/Shared/Jenkins/TestCIAndroid/bin/AndroidManifest.xml : \
+/Users/Shared/Jenkins/TestCIAndroid/AndroidManifest.xml \
---
+++ bin/HelloCI-debug-unaligned.apk.d
@@ -0,0 +1,6 @@
+/Users/Shared/Jenkins/TestCIAndroid/bin/HelloCI-debug-unaligned.apk : \
+/Users/Shared/Jenkins/TestCIAndroid/bin/HelloCI.ap_ \
+/Users/Shared/Jenkins/TestCIAndroid/bin/classes.dex \
+/Users/Shared/Jenkins/appcompat_v7/bin/classes.jar \
+/Users/Shared/Jenkins/appcompat_v7/libs/android-support-v7-appcompat.jar \
+/Users/Shared/Jenkins/appcompat_v7/libs/android-support-v4.jar \
